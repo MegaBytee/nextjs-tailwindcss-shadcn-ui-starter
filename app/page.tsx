5daadf8321f76e4c757f9e491a880e47ee8adeb2
MegaBytee <61125210+MegaBytee@users.noreply.github.com>
2024-09-28 01:25:01 +0100

    v0.0.1 dark mode next-themes integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import DarkModeToggle from "@/components/common/DarkModeToggle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -14,6 +15,7 @@ export default function Home() {
           priority
         />
         <Button>Click me</Button>
+        <DarkModeToggle />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
